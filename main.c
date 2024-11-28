69cce827466a8b332a83320f6d2183ead247b2b5
Brutus <brutuslelik@gmail.com>
2024-11-28 17:33:20 +0300

    key

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 
 #define FIELD_HEIGTH 25
 #define FIELD_LENGTH 80
@@ -55,19 +56,55 @@ void drawField(int key1_position, int key2_position, int ball_position_x, int ba
     
     return;
 }  
-int main() {
-#ifdef DRAWFIELD_DEBUG 
-    drawField(10, 20, 56, 12, 7, 12);
-    printf("pole(10, 20, 56, 12, 7, 12);\n");
-    getchar();
 
-    drawField(24, 7, 43, 8, 1, 17);
-    printf("pole(24, 7, 43, 8, 1, 17);\n");
-    getchar();
+int keyMoving(int key_position, char click){    
+    if ((click == 'a' || click == 'k') && key_position < FIELD_HEIGTH){
+        return key_position + 1;
+    }
+    else if ((click == 'z' || click == 'm') && key_position > 3){
+        return key_position - 1;
+    }
+    else {
+        return key_position;
+    }
+    /*if (click == 'k' && key2_position < FIELD_HEIGTH){
+        return key2_position + 1;
+    }
+    else if (click == 'm' && key2_position > 2){
+        return key2_position - 1;
+    }
+    else {
+        return key2_position;
+    } */   
+
+}
+int main() {
+    int key1_position = 10;
+    int key2_position = 24;
+    char click;
+#ifdef DRAWFIELD_DEBUG 
+    drawField(key1_position, key2_position, 56, 12, 7, 12);
+    printf("pole(10, 20, 56, 12, 7, 12);\n");
+    while (1){
+    click = _getch();
+    if (click == 'a' || click == 'z'){
+    key1_position = keyMoving(key1_position, click);
+    }
+    else if (click == 'k' || click == 'm'){ 
+    key2_position = keyMoving(key2_position, click);
+    }
+    //getchar();
+    drawField(key1_position, key2_position, 56, 12, 0, 0);
+    //printf("pole(24, 7, 43, 8, 1, 17);\n");
+    char stopGame = 'q';
+    if (click == stopGame)
+    break;
+    }
     
-    drawField(1, 13, 24, 10, 20, 19);
+    
+   /* drawField(1, 13, 24, 10, 20, 19);
     printf("pole(1, 13, 24, 10, 20, 19);\n");
-    getchar();
+    getchar(); */ 
 #endif
 
     return 0;
